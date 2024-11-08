c2cfe5422db4c4bc33f26ef43e963cbd64479599
Juan Carlos <carlos85347@gmail.com>
2024-11-08 19:31:41 +0100

    Exercici completat

---
+++ 4 - Aplicar IVA segons el salari.py
@@ -0,0 +1,14 @@
+
+salari = 30000
+
+if salari < 15000:
+    resultado = salari * (0/100)
+    print(f'{resultado}')
+elif salari < 30000:
+    resultado = salari * (10/100)
+    print(f'{resultado}')
+else:
+    resultado = salari * (21/100)
+    print(f'{resultado}')
+
+    
