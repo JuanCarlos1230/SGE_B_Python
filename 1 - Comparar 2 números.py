5bede0168d961318a20552f06f93cbfcf63c2860
Juan Carlos <carlos85347@gmail.com>
2024-11-08 18:05:03 +0100

    Se han echo dos ejercicios

---
+++ 1 - Comparar 2 números.py
@@ -0,0 +1,12 @@
+
+print("Quin movil es més car entre Samsung i Iphone?")
+
+Samsung = 750
+Iphone = 950
+
+if Iphone < Samsung:
+    print("Iphone es més barat que Samsung.")
+elif Iphone == Samsung:
+    print("Tenen el materix preu.")
+else:
+    print("Iphone es més car.") 
