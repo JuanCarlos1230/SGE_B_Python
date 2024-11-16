c0a763725b461fe469cd101047d317bd84a9311b
Juan Carlos <carlos85347@gmail.com>
2024-11-16 11:22:10 +0100

    1-Exercici amb while

---
+++ 1.1 - Nombres del 1 al 10 con (while).py
@@ -0,0 +1,7 @@
+
+numero = 1  # Si inicialitza a 1 perque el numeros vagin de 1 a 10
+
+while (numero <= 10):   # si el numero es més petit o igual a 10 el bucle contina
+    print(f"Este el valor del nombres en esta iteración {numero}")   # imprimeix el numero per cada iteració
+    numero += 1   # cada vegada que passi per aquí se li sumara un numero y quan arribi a 10 no sumarames dons el bucle harua acabat
+    
