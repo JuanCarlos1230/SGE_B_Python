f85138c541b1f69eab559589eeb8fcc4e7c6c6f4
Juan Carlos <carlos85347@gmail.com>
2024-11-16 19:15:23 +0100

    Suma números fins arriba a 100 acabat

--- 6.0 - Suma fins arriba a 100.py
+++ 6.0 - Suma fins arriba a 100.py
@@ -1,10 +1,10 @@
 
-suma = 0    # variables que acumula la suma
-numero = 0  # Si inicialitza a 1 perque el numeros vagin de 1 a 10
+suma = 0    # Variabla que acumula la suma
+numero = 0  # Sí inicialitza a 0 perque no hi ha numero encara
 
 
-while (suma <= 100):   # si el numero es més petit o igual a 10 el bucle contina
+while (suma <= 100):   # Sí el número es més petit o igual a 10 el bucle continua
     numero = numero + 1
     suma = suma + numero
-    print(f"Este el valor del nombres en esta iteración {suma}")   # imprimeix el numero per cada iteració
+    print(f"Aquest es el valor del número en aquesta iteració {suma}")   # Imprimeix el número per cada iteració
     
