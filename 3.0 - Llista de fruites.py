11ece6897959f4bb5c1b1a5e8a60b99c339a1d12
Juan Carlos <carlos85347@gmail.com>
2024-11-16 11:31:46 +0100

    3-Exercici Lista

---
+++ 3.0 - Llista de fruites.py
@@ -0,0 +1,5 @@
+
+fruites = ["poma","pera","raïm","plàtan"]   # llista de fruites (comprova si hi ha mes fruites)
+for i in fruites:  # Sí la fruita hi esta en i passa fer la impresió                               
+    print(f"Li agraden aquestas fruites {i}")   # i imprimeix les fruites d'una en una per imprimir-ne totes a l'hora canviarem i per fuites
+    
