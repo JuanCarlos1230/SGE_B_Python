654d1e223703a7bf2c8d989034e49079f3f7dc1f
Juan Carlos <carlos85347@gmail.com>
2024-11-16 11:23:05 +0100

    2-Exercici Suma els 10 primers numeros

---
+++ 2.0 - Suma 10 primeros numeros.py
@@ -0,0 +1,8 @@
+
+suma = 0    # variables que acumula la suma
+
+for i in range(1,11):   # Rango del 1 al 10 Realment ya que comença pero 0
+    suma += i   # cada vegada que pasa per qui li sumen i    
+    print(f"La suma de los numeros en cada iteración: {suma}")  # imprimeix la suma de cada iterció   
+print(f"La suma final de los 10 primeros numeros es: {suma}")   # imprimeix la suma total després de termina el bucle for
+    
