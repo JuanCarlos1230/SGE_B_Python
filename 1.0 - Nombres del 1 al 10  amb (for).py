103ff423814920348b36248936d87f234f593848
Juan Carlos <carlos85347@gmail.com>
2024-11-16 11:21:31 +0100

    1-Exercici amb for

---
+++ 1.0 - Nombres del 1 al 10  amb (for).py
@@ -0,0 +1,5 @@
+
+numero = [1,2,3,4,5,6,7,8,9,10]    # llista de numeros   
+for i  in numero:   # si el numero esta en i seguira bucle fins que no hihagi cap numero 
+    print(f"Aquest es el valor del numero en aquesta iteració {i}")    # imprimeix el numeros fins que acabi el bucle
+     
